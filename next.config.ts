43a1c357d6bc776645dbc318864d11887a436b5e
Ping Silva <efjanphotography@gmail.com>
2025-07-30 07:29:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/v1/create-qr-code/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'qisldnceqvfcqvkzsvrd.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      }
     ],
   },
 };
