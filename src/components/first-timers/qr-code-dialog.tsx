463cf5f15d552da372c19420a44191b93e65c5d6
Ping Silva <efjanphotography@gmail.com>
2025-08-05 15:13:48 +0000

    also include the name below the qr code

--- src/components/first-timers/qr-code-dialog.tsx
+++ src/components/first-timers/qr-code-dialog.tsx
@@ -100,4 +100,3 @@ export default function QrCodeDialog({ firstTimer, children }: QrCodeDialogProps
     </Dialog>
   );
 }
-
--- src/components/members/qr-code-dialog.tsx
+++ src/components/members/qr-code-dialog.tsx
@@ -100,4 +100,3 @@ export default function QrCodeDialog({ member, children }: QrCodeDialogProps) {
     </Dialog>
   );
 }
-
