b5006c49a51696f05d3cb8b5677449c7e36c66b6
Roseller Armirola <roseller@fusionict.com>
2025-08-19 14:58:56 +0000

    now it's just looping

---
+++ src/next.config.ts
@@ -0,0 +1,36 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  basePath: '/expoattendance',
+  trailingSlash: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/v1/create-qr-code/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'qisldnceqvfcqvkzsvrd.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
