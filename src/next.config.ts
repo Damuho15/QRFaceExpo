16a99191128e5c8430ec0588a8e0791f18a53958
Roseller Armirola <roseller@fusionict.com>
2025-08-19 15:13:11 +0000

    did you write the changes tho, cos i'm seeing nothing has been altered.

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,8 +1,8 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/expoattendance',
-  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
