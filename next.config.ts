7aa51f7e4126c516b7a177ae79cc84cb8ff482e7
Roseller Armirola <roseller@fusionict.com>
2025-08-24 11:33:36 +0000

    i don't need guesswork. i want it done systematically. if you can't even

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/v1/create-qr-code/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'qisldnceqvfcqvkzsvrd.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      }
     ],
   },
 };
